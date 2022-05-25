73f20871cafb935161ac9a2e437b37714bb1ceac
Anthony Connell <toco85@gmail.com>
2022-05-25 17:18:08 -0400

    Added Delete and Edit elements

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,9 +6,9 @@ function edit_form (data) {
         <Def>
           <main>
                 <h1>Edit Place</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                 <div className="row">
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="name">Place Name*</label>
                         <input className="form-control" 
                                 id="name" 
@@ -17,7 +17,7 @@ function edit_form (data) {
                                 required
                                 />
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="pic">Place Picture</label>
                         <input className="form-control" 
                                 id="pic" 
@@ -27,7 +27,7 @@ function edit_form (data) {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="city">City</label>
                         <input className="form-control" 
                                 id="city" 
@@ -35,7 +35,7 @@ function edit_form (data) {
                                 value={data.place.city}
                                 />
                     </div>
-                    <div className="form-group col-sm-6">
+                    <div className="form-group col-sm-4">
                         <label htmlFor="state">State</label>
                         <input className="form-control" 
                                 id="state" 
@@ -53,7 +53,15 @@ function edit_form (data) {
                                 required
                                 />
                     </div>
-                    <input className="btn btn-primary" type="submit" value="Edit Place" />
+                    <div className="form-group col-sm-4">
+                        <label htmlFor="founded">Founded</label>
+                        <input className="form-control"
+                            id="founded"
+                            name="founded"
+                            value={data.place.founded}
+                        />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Update Place" />
                 </form>
           </main>
         </Def>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,20 +7,25 @@ function show (data) {
       No comments yet!
     </h3>
   )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
   if (data.place.comments.length) {
-    comments = data.place.comments.map(c => {
-      return (
-        <div className="border">
-          <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-          <h4>{c.content}</h4>
-          <h3>
-            <strong>- {c.author}</strong>
-          </h3>
-          <h4>Rating: {c.stars}</h4>
-        </div>
-      )
-    })
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐'
   }
+  rating = (
+    <h3>
+      {stars} stars
+    </h3>
+  )
   return (
       <Def>
         <main>
@@ -36,8 +41,7 @@ function show (data) {
                     <h2>
                       Rating
                     </h2>
-                    No ratings yet
-                    <br />
+                    {rating}
                     <h2>
                       Description
                     </h2>
@@ -92,7 +96,7 @@ function show (data) {
             </form>
         </main>
       </Def>
-  )
+    )
+  }
 }
-
 module.exports = show
